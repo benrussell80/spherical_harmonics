25d29b0bcc952a83be2b5757d15f93a743215a41
benrussell80 <bprussell80@gmail.com>
2020-01-03 13:22:47 -0800

    Prepared for deployment to AWS EC2 instance

--- app/main.py
+++ app/main.py
@@ -3,8 +3,9 @@ from functools import lru_cache
 import numpy as np
 from bokeh.io import curdoc
 from bokeh.layouts import column, row, widgetbox
-from bokeh.models import (ColorBar, ColumnDataSource, ContinuousTicker,
-                          HoverTool, Label, LinearColorMapper, Slider, Button, Panel, Tabs)
+from bokeh.models import (Button, ColorBar, ColumnDataSource, ContinuousTicker,
+                          HoverTool, Label, LinearColorMapper, Panel, Slider,
+                          Tabs)
 from bokeh.palettes import Greys, Viridis
 from bokeh.plotting import figure
 
@@ -16,8 +17,8 @@ l, m = 3, 2
 z = 1.5
 
 # Color Maps
-continuous = Greys[256]
-discrete = Viridis[11]
+greys = Greys[256]
+viridis = Viridis[11]
 
 # Plotting Data
 @lru_cache
@@ -35,34 +36,33 @@ source = ColumnDataSource({'image': [0], 'x': [0], 'y': [0], 'dw': [num], 'dh': 
 source.data['image'] = update_plot(l, m, z)
 
 # Set up figure
-plot_width = 750
-plot_height = 750
-hover = HoverTool(tooltips=[('Value: ', '@image')])
-p1 = figure(plot_width=plot_width, plot_height=plot_height, x_range=(0, num), y_range=(0, num), toolbar_location=None, match_aspect=True, aspect_scale=1, sizing_mode="scale_both", tools="")
+hover = HoverTool(tooltips=[(f'Y_lm: ', '@image{0.2f}')])
+p1 = figure(x_range=(0, num), y_range=(0, num), toolbar_location=None, match_aspect=True, aspect_scale=1, tools="", css_classes=["figure"])
 p1.add_tools(hover)
 p1.axis.visible = False
-color_mapper = LinearColorMapper(palette=continuous, low=-0.6, high=0.6)
+color_mapper = LinearColorMapper(palette=greys, low=-0.6, high=0.6)
 p1.image(image='image', source=source, x='x', y='y', dw='dw', dh='dh', color_mapper=color_mapper)
-button = Button(label="Color Palette: Continuous", button_type="default")
+button = Button(label="Color Palette: Greys", css_classes=['btn-grey'])
 
 def button_on_click():
-    if color_mapper.palette == continuous:
-        color_mapper.palette = discrete
-        button.label = "Color Palette: Discrete"
-        button.button_type = "success"
-    else:
-        color_mapper.palette = continuous
-        button.label = "Color Palette: Continuous"
-        button.button_type = "default"
+    if color_mapper.palette == greys:
+        color_mapper.palette = viridis
+        button.label = "Color Palette: Viridis"
+        button.css_classes = ['btn-viridis']
 
+    else:
+        color_mapper.palette = greys
+        button.label = "Color Palette: Greys"
+        button.css_classes = ['btn-grey']
 
 button.on_click(button_on_click)
 
+
 # Set up widgets
 nlm_label = Label(x=0, y=0, text="Quantum Numbers", render_mode="canvas")
-l_slider = Slider(title="l", value=l, start=0, end=l_max, step=1, height=250)
-m_slider = Slider(title="m", value=m, start=-l, end=l, step=1, height=250)
-z_slider = Slider(title='z', value=z, start=-10, end=10, step=0.1, height=250)
+l_slider = Slider(title="l", value=l, start=0, end=l_max, step=1) # , height=250)
+m_slider = Slider(title="m", value=m, start=-l, end=l, step=1) # , height=250)
+z_slider = Slider(title='z', value=z, start=-10, end=10, step=0.1) # , height=250)
 
 
 def update_on_l_change(attrname, old, new):
@@ -106,13 +106,15 @@ def update_on_z_change(attrname, old, new):
 z_slider.on_change('value', update_on_z_change)
 
 
-inputs = column(z_slider, l_slider, m_slider, button, sizing_mode="scale_both")
-inputs.max_height = 300
+inputs = column(z_slider, l_slider, m_slider, button, sizing_mode="scale_width")
 
-tab1 = Panel(child=row(inputs, p1), title="2D Heatmap")
+p1.sizing_mode = "scale_both"
 
-tabs = Tabs(tabs=[tab1])
+output = row(inputs, p1, sizing_mode="scale_both")
 
-curdoc().add_root(tabs)
+output.margin = 0
+output.spacing = 0
+
+curdoc().add_root(output)
 
 curdoc().title = "Spherical Harmonics"

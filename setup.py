f8f957ad001ced8a064384783df9d378eb031b7d
benrussell80 <bprussell80@gmail.com>
2020-01-08 08:56:45 -0800

    Update 0.0.2: Fixed mistake in sh func for l=4, m=2.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spherical_harmonics",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     entry_points={},
     python_requires='>=3.6',
